d4bfa338105b1c9edcdf942feb49b75c2293cc0a
Ralph Greschner <ralph.greschner@gmail.com>
2019-03-16 18:32:37 +0100

    improvement: uses buckets for sensor data persistence. BREAKING CHANGE: only buckets are supported from now on.

--- packages/api/src/sensor-data/sensor-data.repository.ts
+++ packages/api/src/sensor-data/sensor-data.repository.ts
@@ -1,27 +1,40 @@
 import { BaseRepository } from '../persistence/base.repository';
 import { GenericSensorData } from './generic-sensor-data.model';
 import { MeanAcceleration } from '../statistics/mean-acceleration.model';
+import * as moment from 'moment';
 
 /**
  * Repository for Sensor Data.
  */
 export class SensorDataRepository extends BaseRepository<GenericSensorData> {
-  static COLLECTION_NAME = 'sensorData';
+  static COLLECTION_NAME = 'sensorDataBuckets';
 
   /**
-   * Create pipeline to aggregate mean acceleration
-   * in date range.
-   * @param startDate Start date of date range.
-   * @param endDate End date of date range.
-   */
+	 * Create pipeline to aggregate mean acceleration
+	 * in date range.
+	 * @param startDate Start date of date range.
+	 * @param endDate End date of date range.
+	 */
   private createPipelineForMeanAcceleration(startDate: Date, endDate: Date) {
     const pipeline = [
+      // Match on bucket.
+      {
+        $match: {
+          endDate: { $gte: startDate, $lte: endDate }
+        }
+      },
+      // Focus on sample data only by 
+      // unwind & replace root.
+      { $unwind: '$samples' },
+      { $replaceRoot: { newRoot: '$samples' } },
+      // Match on sample.
       {
         $match: {
           'type': 'device-motion',
           'payload.ts': { $gte: startDate, $lte: endDate }
         }
       },
+      // Group axis data.
       {
         $group: {
           _id: null,
@@ -30,16 +43,17 @@ export class SensorDataRepository extends BaseRepository<GenericSensorData> {
           z: { $avg: '$payload.z' }
         }
       },
+      // Id neither necessary nor useful in result.
       { $project: { _id: false } }
     ];
     return pipeline;
   }
 
   /**
-   * Get mean acceleration in date range.
-   * @param startDate Start date of date range.
-   * @param endDate End date of date range.
-   */
+	 * Get mean acceleration in date range.
+	 * @param startDate Start date of date range.
+	 * @param endDate End date of date range.
+	 */
   public async getMeanAccelerationInDateRange(
     startDate: Date,
     endDate: Date
@@ -50,7 +64,10 @@ export class SensorDataRepository extends BaseRepository<GenericSensorData> {
     if (!endDate) {
       throw new Error('Invalid end date.');
     }
-    const pipeline = this.createPipelineForMeanAcceleration(startDate, endDate);
+    const pipeline = this.createPipelineForMeanAcceleration(
+      startDate,
+      endDate
+    );
     const aggregationResults = await this.aggregate(pipeline);
     const meanAcceleration = aggregationResults[0];
     return Promise.resolve(meanAcceleration);
@@ -62,7 +79,24 @@ export class SensorDataRepository extends BaseRepository<GenericSensorData> {
     );
   }
 
+  /**
+   * Add new sensor data from device.
+   * @param sensorData New sensor data.
+   */
   public addSensorData(sensorData: GenericSensorData) {
-    return this.insertOne(sensorData);
+    const ts = sensorData.payload.ts;
+    const day = moment(ts).format('YYYY-MM-DD');
+    const updateDoc = {
+      $set: { day },
+      $min: { startDate: ts },
+      $max: { endDate: ts },
+      $push: { samples: sensorData },
+      $inc: { nsamples: 1 }
+    };
+    return this.collection.updateOne(
+      { day, nsamples: { $lt: 9000 } },
+      updateDoc,
+      { upsert: true }
+    );
   }
 }
